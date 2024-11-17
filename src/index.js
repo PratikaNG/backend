37ce21146bf44e32b00a6b2a78ef15c2529eac01
Pratika NG <pratika.05ng@gmail.com>
2024-11-17 12:59:01 +0530

    connecting to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:"./env"
+})
+/*now change the dev script to this
+nodemon -r dotenv/config --experimental-json-modules src/index.js
+
+we are doing this to bring consistency in the code while importing dotenv temporarily until the dotenv package brings the import syntax permanently into it.
+*/
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+basic approach
+import mongoose from "mongoose"
+import { DB_NAME } from "./constant";
+import express from "express"
+const app = express()
+
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(err)=>{
+            console.log("ERROR",err)
+            throw err
+        })
+        app.listen(`${process.env.PORT}`,()=>{
+            console.log(`App is listeining on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("error",error)
+        throw error
+    }
+})()
+*/
